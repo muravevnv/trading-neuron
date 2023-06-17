f8f1aa6431c664ab313645a6099ac8fcf25d2d4e
Nickolay Muravev <murvievnick@gmail.com>
2023-06-17 15:51:49 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -91,37 +91,11 @@ burger.addEventListener('click', function() {
 })
 
 
-    function setCookie(name, value, days) {
-        let expires = "";
-        if (days) {
-            let date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/";
-    }
-    function getCookie(name) {
-        let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-    }
-    function checkCookies() {
-        let cookieNote = document.getElementById('cookie_note');
-        let cookieBtnAccept = cookieNote.querySelector('.cookie_accept');
-        // Если куки cookies_policy нет или она просрочена, то показываем уведомление
-        if (!getCookie('cookies_policy')) {
-            cookieNote.classList.add('show');
-        }
-        cookieBtnAccept.addEventListener('click', function () {
-            setCookie('cookies_policy', 'true', 365);
-            cookieNote.classList.remove('show');
-        });
-    }
-    checkCookies();
 
     let scrollBtn = document.querySelector('.scroll-top');
 
     document.addEventListener('scroll', function(){
-      if(window.pageYOffset > 1500) {
+      if(window.pageYOffset > 700) {
         scrollBtn.classList.add('is-visible')
       } else {
         scrollBtn.classList.remove('is-visible');
@@ -167,3 +141,32 @@ overlay.forEach(function (item) {
 
   });
 }); 
+
+
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function checkCookies() {
+  let cookieNote = document.getElementById('cookie_note');
+  let cookieBtnAccept = cookieNote.querySelector('.cookie_accept');
+  // Если куки cookies_policy нет или она просрочена, то показываем уведомление
+  if (!getCookie('cookies_policy')) {
+      cookieNote.classList.add('show');
+  }
+  cookieBtnAccept.addEventListener('click', function () {
+      setCookie('cookies_policy', 'true', 365);
+      cookieNote.classList.remove('show');
+  });
+}
+checkCookies();
